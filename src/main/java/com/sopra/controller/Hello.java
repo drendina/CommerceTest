c829213e6a9f4ea2ecf67ab1877f757e014dffd7
Daniele Rendina <daniele.rendina@soprasteria.com>
2018-04-10 17:43:05 +0200

    Controller and Log added

---
+++ src/main/java/com/sopra/controller/Hello.java
@@ -0,0 +1,21 @@
+package com.sopra.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.apache.log4j.Logger;
+
+@Controller
+public class Hello {
+    private static final Logger logger = Logger.getLogger(Hello.class);
+
+    @RequestMapping(method = RequestMethod.GET, value = "/index")
+    public ModelAndView getPage() {
+        ModelAndView mv = new ModelAndView("index");
+        mv.addObject("greetings", "hello there!");
+        logger.info("Create page index.jsp");
+
+        return mv;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>English</title>
+</head>
+<body>
+${greetings}
+
+<% System.out.println("Prova Jsp"); %>
+</body>
+</html>
